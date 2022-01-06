afcc5fa76a075d15362c23f64e1b9ddf52e75c0b
Alp Tarla <alptarla@gmail.com>
2022-01-06 17:45:35 +0300

    Update navbar brand

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,7 @@ export default {
     <b-navbar class="container">
       <template #brand>
         <b-navbar-item tag="router-link" to="/">
-          <span>AskMe</span>
+          <span class="brand">AskMe</span>
           <i class="fas fa-question fa-2x" />
         </b-navbar-item>
       </template>
@@ -29,5 +29,9 @@ export default {
 .header {
   padding: 1rem 0;
   border-bottom: 2px solid hsl(0, 0%, 96%);
+}
+
+.brand {
+  font-size: 24px;
 }
 </style>

5b049e827d1f93ccbda322cbf02dbdec3f58484d
Alp Tarla <alptarla@gmail.com>
2022-01-07 13:21:03 +0300

    Add navigation guards for authorization

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
@@ -10,17 +11,29 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.isLoggedIn) next()
+      else next({ path: '/auth/sign-in' })
+    }
   },
   {
     path: '/auth/sign-in',
     name: 'SignIn',
-    component: SignIn
+    component: SignIn,
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.isLoggedIn) next({ path: '/' })
+      else next()
+    }
   },
   {
     path: '/auth/sign-up',
     name: 'SignUp',
-    component: SignUp
+    component: SignUp,
+    beforeEnter: (to, from, next) => {
+      if (store.state.auth.isLoggedIn) next({ path: '/' })
+      else next()
+    }
   }
 ]
 

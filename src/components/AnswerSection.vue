d3ddc2464e55870a1aecc8f415ca7a82983d0e44
Alp Tarla <alptarla@gmail.com>
2022-01-07 22:29:54 +0300

    Update AnswerSection.vue

--- src/components/AnswerSection.vue
+++ src/components/AnswerSection.vue
@@ -1,34 +1,74 @@
 <script>
+import moment from 'moment'
+import { mapState } from 'vuex'
 import SectionCard from './SectionCard.vue'
 import VoteButton from './VoteButton.vue'
+
 export default {
   name: 'AnswerSection',
   components: { SectionCard, VoteButton },
+  props: {
+    answer: {
+      type: Object,
+      requried: true
+    },
+    answerTo: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState('auth', ['user']),
+    dateFromNow() {
+      return moment(this.answer.createdAt).fromNow()
+    },
+    isAlreadyVoted() {
+      return this.answer.votes.includes(this.user.id)
+    },
+    isOwner() {
+      return this.answer.user.id === this.user.id
+    }
+  },
   methods: {
     handleUpdateVote() {
       // todo: update answer vote count
+    },
+    handleDeleteAnswerClick() {
+      // todo: delete this answer
+    },
+    handleEditAnswerClick() {
+      // todo: edit this answer
     }
   }
 }
 </script>
 
 <template>
-  <SectionCard username="Alp Tarla" date="2 hours ago">
+  <SectionCard :username="answer.user.username" :date="dateFromNow" :is-owner="isOwner">
     <template #card-left>
-      <VoteButton :vote="10" @update-vote="handleUpdateVote" :is-already-voted="false" />
+      <VoteButton
+        :vote="answer.votes.length"
+        @update-vote="handleUpdateVote"
+        :is-already-voted="isAlreadyVoted"
+      />
     </template>
-    <template #card-action class="ml-auto">
+    <template v-if="isOwner" #card-action class="ml-auto">
       <div class="is-flex is-align-items-center">
-        <b-button type="is-ghost" class="mr-2 has-text-danger">
+        <b-button type="is-ghost" class="mr-2 has-text-danger" @click="handleDeleteAnswerClick">
           <i class="fas fa-trash-alt mr-2" />
           <span>Delete</span>
         </b-button>
-        <b-button type="is-ghost">
+        <b-button type="is-ghost" @click="handleEditAnswerClick">
           <i class="fas fa-pencil-alt mr-2" />
           <span>Edit</span>
         </b-button>
       </div>
     </template>
-    <template #card-description> This is a answer for this question </template>
+    <template #card-description>
+      <p>
+        <span class="has-text-primary mr-2">@{{ answerTo }}</span>
+        <span>{{ answer.answer }}</span>
+      </p>
+    </template>
   </SectionCard>
 </template>
